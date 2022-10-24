ca1a361852d0b5b6efed8f98dd5a151759566fa0
Andreezzy <frandres333@gmail.com>
2022-10-24 00:01:12 -0500

    Solution #2 of valid anagram

---
+++ 2-ValidAnagramSol2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+#define NUM_CHARS 26
+
+bool is_anagram(string str1, string str2) {
+    
+    if(str1.length() != str2.length()) return false;
+    
+    int count[NUM_CHARS] = { 0 } ; // [0,0,0,....0] -> len(26)
+    
+    for(int i = 0; i < str1.length(); i++) {
+        int index = str1[i] - 'a';
+        int index2 = str2[i] - 'a';
+
+        count[index] += 1;
+        count[index2] -= 1;
+    }
+    // count1 = [3, 0, 0, 0... 1,. ...1 ,..0]
+    // count2= [3, 1, 1, 1... 1,. ...1 ,..0]
+    // count1[0] == count2[0] | true
+    // 3 == 3 = true
+    
+    for(int j = 0; j < NUM_CHARS; j++) {
+        if(count[j] != 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+int main()
+{
+    string s = "anagram";
+    string t = "nagaram";
+    
+    cout << is_anagram(s, t) << endl;
+
+    return 0;
+}
